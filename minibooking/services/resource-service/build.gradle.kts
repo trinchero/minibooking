c654bc8e2e620e183ab230e0b88d8a251af9ce8d
Andrea <andreatrinchero92@gmail.com>
2025-08-31 13:15:14 +0200

    added lib to gradle build

--- minibooking/services/resource-service/build.gradle.kts
+++ minibooking/services/resource-service/build.gradle.kts
@@ -1,7 +1,14 @@
-plugins { id("org.springframework.boot"); id("io.spring.dependency-management") }
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
 dependencies {
     implementation(project(":libs:common-domain"))
+
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }

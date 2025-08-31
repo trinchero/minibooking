2b2e4963ea73c9c1a08fd380dfb1e83fed64ddf9
Andrea <andreatrinchero92@gmail.com>
2025-08-31 12:11:07 +0200

    added Main

---
+++ minibooking/services/booking-service/src/main/java/com/minibooking/booking/BookingServiceApplication.java
@@ -0,0 +1,10 @@
+package com.minibooking.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication @EnableScheduling
+public class BookingServiceApplication {
+    public static void main(String[] args){ SpringApplication.run(BookingServiceApplication.class,args); }
+}

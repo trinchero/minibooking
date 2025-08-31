a6181133525f3fe12437416d2845ce78267f7954
Andrea <andreatrinchero92@gmail.com>
2025-08-31 12:24:29 +0200

    added main

---
+++ minibooking/services/resource-service/src/main/java/com/minibooking/resource/ResourceServiceApplication.java
@@ -0,0 +1,5 @@
+package com.minibooking.resource;
+import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.boot.SpringApplication;
+@SpringBootApplication public class ResourceServiceApplication {
+    public static void main(String[] args){ SpringApplication.run(ResourceServiceApplication.class,args); }
+}

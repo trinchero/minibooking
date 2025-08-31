224a74c18e975645db308f1ce94d7e73e9fb5bc3
Andrea <andreatrinchero92@gmail.com>
2025-08-31 13:15:56 +0200

    added lib to gradle build

--- minibooking/services/orchestrator-service/src/main/java/com/minibooking/orchestrator/OrchestratorServiceApplication.java
+++ minibooking/services/orchestrator-service/src/main/java/com/minibooking/orchestrator/OrchestratorServiceApplication.java
@@ -1,5 +1,6 @@
 package com.minibooking.orchestrator;
-import org.springframework.boot.autoconfigure.SpringBootApplication; import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 @SpringBootApplication public class OrchestratorServiceApplication {
     public static void main(String[] args){ SpringApplication.run(OrchestratorServiceApplication.class,args); }
 }
